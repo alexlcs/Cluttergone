d4028dca118bbb88f462dab51d7d6164a04e5f14
alexlcs <alexlcs.git@gmail.com>
2016-07-31 09:40:08 -0400

    remove

--- cluttergone.js
+++
@@ -1,5 +0,0 @@
-// remove suggested videos on start screen
-document.querySelector('#feed').style.display = 'none';
-
-// remove navbar under search bar
-document.querySelector('#masthead-appbar-container').style.display = 'none';
