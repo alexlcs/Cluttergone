befa2201ae5092c8f81c55ff63166d68e47e2356
alexlcs <alexlcs.git@gmail.com>
2016-07-31 09:42:23 -0400

    Initial clean
    Cleanup feed and second navbar

---
+++ cluttergone.js
@@ -0,0 +1,5 @@
+// remove suggested videos on start screen
+document.querySelector('#feed').style.display = 'none';
+
+// remove navbar under search bar
+document.querySelector('#masthead-appbar-container').style.display = 'none';
